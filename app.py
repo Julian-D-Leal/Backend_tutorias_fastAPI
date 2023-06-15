042e7df56b4f7570f7f0c5230e764dd742d86754
Julian-D-Leal <leal.julian@correounivalle.edu.co>
2023-06-15 18:26:00 -0500

    version inicial

---
+++ app.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Tutorias FastAPI",
+    description= "API desarrollada para el proyecto de trabajo de grado de la Universidad del Valle",
+    version="0.0.1",
+    
+)
